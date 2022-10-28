b2d48fcfede7e13cd20ac7e1d44c84c2b0f37248
devhabeeb <77229868+devhabeeblateef@users.noreply.github.com>
2022-10-28 23:44:09 +0100

    Footer please edit with bootstrap - Hopefully, I'll check it when i'm done by 2AM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Profile from './components/Profile';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='place-content-center'>
         <Profile/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='xl:flex xl:space-x-96 border w-screen'>
+        <div className='border'>Hello</div>
+        <div className='border'>World</div>
+        <div className='border'>!</div>
+    </div>
+  )
+}
+
+export default Footer
