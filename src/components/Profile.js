3f8b29cf3ee82dc585eeed292cb856f01e919c90
devhabeeb <77229868+devhabeeblateef@users.noreply.github.com>
2022-10-28 23:01:59 +0100

    My commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,21 +2,18 @@ import React from 'react'
 import profileimg from './img/profile.png';
 function Profile() {
   return (
-    <div> 
+    <div className='font-inter'> 
     <div className='flex justify-center pt-8'>
         <img className="w-32 rounded-full items-center" src={profileimg} alt="logo"/>
-        <svg className=" rounded-full border p-2 absolute  ml-[40%]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-    </svg>
     </div>
     
     <div className='grid grid-flow-row justify-center auto-rows-max'>
-        <div className='rounded-[30px] text-center w-96 my-6 bg-[#EAECF0]'>1</div>
-        <div className='rounded-[30px] text-center w-96 my-6 bg-[#EAECF0]'>1</div>
-        <div className='rounded-[30px] text-center w-96 my-6 bg-[#EAECF0]'>1</div>
-        <div className='rounded-[30px] text-center w-96 my-6 bg-[#EAECF0]'>1</div>
-        <div className='rounded-[30px] text-center w-96 my-6 bg-[#EAECF0]'>1</div>
-        <div className='rounded-[30px] text-center w-96 my-6 bg-[#EAECF0]'>1</div>
+        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Twitter Link</div>
+        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Zuri Team</div>
+        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Zuri Books</div>
+        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Python Books</div>
+        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Background Check for Coders</div>
+        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Design Books</div>
     </div> 
     </div>
   )
