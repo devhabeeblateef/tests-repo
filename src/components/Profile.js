961ba0d18804434b90f96878814151d94ac1096a
devhabeeb <77229868+devhabeeblateef@users.noreply.github.com>
2022-10-28 23:16:44 +0100

    Adjusted the width of divs and border radius

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,13 +7,14 @@ function Profile() {
         <img className="w-32 rounded-full items-center" src={profileimg} alt="logo"/>
     </div>
     
-    <div className='grid grid-flow-row justify-center auto-rows-max'>
-        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Twitter Link</div>
-        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Zuri Team</div>
-        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Zuri Books</div>
-        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Python Books</div>
-        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Background Check for Coders</div>
-        <div className='rounded-[30px] p-4 text-center w-96 my-6 bg-[#EAECF0]'>Design Books</div>
+    <div className='grid items-center px-16 w-screen'>
+        <div className='rounded-[10px] p-4 text-center w-[100%] my-6 bg-[#EAECF0]'>Twitter Link</div>
+        <div className='rounded-[10px] p-4 text-center w-[100%] my-6 bg-[#EAECF0]'>Twitter Link</div>
+        <div className='rounded-[10px] p-4 text-center w-[100%] my-6 bg-[#EAECF0]'>Twitter Link</div>
+        <div className='rounded-[10px] p-4 text-center w-[100%] my-6 bg-[#EAECF0]'>Twitter Link</div>
+        <div className='rounded-[10px] p-4 text-center w-[100%] my-6 bg-[#EAECF0]'>Twitter Link</div>
+        <div className='rounded-[10px] p-4 text-center w-[100%] my-6 bg-[#EAECF0]'>Twitter Link</div>
+        
     </div> 
 
     
